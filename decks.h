cdc9d62044c98b77e56873498668fb2f84359716
Tim O'Brien <60678896+tobri008@users.noreply.github.com>
2020-02-11 20:18:18 -0500

    Update decks.h

--- decks.h
+++ decks.h
@@ -1,31 +1,20 @@
-#ifndef PLAYER_H
-#define PLAYER_H
+#ifndef DECKS_H
+#define DECKS_H
 
 #include "cards.h"
-#include "decks.h"
+#include <vector>
 
-struct Player {
-  Card	myCards[3];
-  int 	myScore;
-  int 	removedCard;
-};
+typedef std::vector<Card> Deck;
 
+Card draw (Deck& d);
+// remove and return one card from the end of the deck
 
+void fillDeck (Deck&  d);
+// initialize a deck by creating all 52 cards
 
-void addPoints (Player& p, int howMany);
-// add the given number of points to the current score
+bool	isEmpty (const Deck& d );
 
-void dealHandTo (Player& p, Deck& aDeck);
-// initialize the data fields for a player
-
-Card draw (Player& p);
-// return a random card from our hand
-
-void replaceCard (Player& p, Deck& aDeck);
-// replace last card played with new card
-
-int score (const Player& p);
-// return the current score
+void shuffle (Deck& deck);
 
 
 #endif
